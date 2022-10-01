c460b65c95330f4b5ba83039e903211cf7fd2ea7
Maksym Zdorovets <mz@extracode.com.ua>
2022-10-01 20:15:07 +0300

    #UnitOfWork for Sql

--- TaskList.Domain/UnitOfWorks/Abstract/IRepository.cs
+++ TaskList.Domain/UnitOfWorks/Abstract/IRepository.cs
@@ -3,7 +3,7 @@
 public interface IRepository<TEntity> : IDisposable where TEntity : class
 {
     Task Add(TEntity obj);
-    Task<TEntity> GetById<TKey>(TKey id);
+    Task<TEntity?> GetById<TKey>(TKey id);
     Task<IEnumerable<TEntity>> GetAll();
     Task Update(TEntity obj);
     Task Remove<TKey>(TKey id);
