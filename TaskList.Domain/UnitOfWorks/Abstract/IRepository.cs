1297b0726b9bdbb54a206836654f532cb9994dce
Олег Самодрыга <osd@extracode.com.ua>
2022-10-01 14:04:00 +0300

    fixed repository and unitOfWork

--- TaskList.Domain/UnitOfWorks/Abstract/IRepository.cs
+++ TaskList.Domain/UnitOfWorks/Abstract/IRepository.cs
@@ -1,10 +1,10 @@
 ﻿namespace TaskList.Domain.UnitOfWorks.Abstract;
 
-public interface IRepository<TEntity, TKey> : IDisposable where TEntity : class
+public interface IRepository<TEntity> : IDisposable where TEntity : class
 {
     Task Add(TEntity obj);
-    Task<TEntity> GetById(TKey id);
+    Task<TEntity> GetById<TKey>(TKey id);
     Task<IEnumerable<TEntity>> GetAll();
     Task Update(TEntity obj);
-    Task Remove(TKey id);
+    Task Remove<TKey>(TKey id);
 }
--- TaskList.Domain/UnitOfWorks/Abstract/IUnitOfWork.cs
+++ TaskList.Domain/UnitOfWorks/Abstract/IUnitOfWork.cs
@@ -2,5 +2,6 @@
 
 public interface IUnitOfWork : IDisposable
 {
+    IRepository<TaskList.Domain.Model.Task> TaskRepository { get; set; }
     bool Commit();
 }
